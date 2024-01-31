c489b3cc2a095e35e0894461695d033a3e343083
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-02-01 01:47:09 +0600

    start the loop.js

---
+++ for-of.js
@@ -0,0 +1,18 @@
+const numbers=[12,35,34,38,39,56];
+ 
+// console.log(numbers);
+
+
+// for(const num of numbers){
+//             console.log(num);
+// }
+
+
+
+let names=['ashim', 'nirab', 'shaon', 'muzahid', 'antor'];
+
+
+for(let name of names){
+            console.log(name);
+            console.log('sbai porte boso')
+}
