92fc7aa08ca7ff5632ea9b4cefaa0be129af0d22
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-02-01 02:18:38 +0600

    i am working while loop

---
+++ while.js
@@ -0,0 +1,12 @@
+let num =7;
+
+while(num<10){
+            console.log('coding korte hobe prochur', num);
+            num++;
+}
+
+let total=300;
+while(total<500){
+         console.log('even number', total)
+         total+=10;
+}
