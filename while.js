2a811b127eb1354b2e993a0dfeb338f1d9cd3b72
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-02-01 02:33:57 +0600

    i learn how to do while loop sum

--- while.js
+++ while.js
@@ -10,3 +10,12 @@ while(total<500){
          console.log('even number', total)
          total+=10;
 }
+
+let number=100;
+let sum =0;
+while(number<200){
+            console.log(number);
+            sum=sum+number;
+            console.log('sum:', sum)
+            number+=5;
+}
